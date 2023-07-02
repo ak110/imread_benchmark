0f18ec0f6d26dbd01b8a3c798e7c3456d58dc76b
wakamezake <hotwater1367@gmail.com>
2023-07-02 19:55:31 +0900

    update

--- imread_benchmark.py
+++ imread_benchmark.py
@@ -1,39 +1,57 @@
 #!/usr/bin/env python3
 import sys
 import platform
+import logging
+import timeit
+
+from pathlib import Path
+
 import PIL.Image
 import cv2
 import imageio
 import jpeg4py
-import lycon
 import numpy as np
 import skimage
 import skimage.color
 import skimage.io
 import tensorflow as tf
-import timeit
 import torch
 import torchvision
-from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
-IMG_DIR_NAME = 'images'
+IMG_DIR_NAME = "images"
 IMG_PATH = BASE_DIR / IMG_DIR_NAME
-X1 = list(path for path in IMG_PATH.glob('*') if path.suffix != ".gif")
+X1 = list(path for path in IMG_PATH.glob("*") if path.suffix != ".gif")
+
+logging.basicConfig(level=logging.INFO)
+Logger = logging.getLogger(__name__)
+
+image_libs = [
+    np,
+    cv2,
+    PIL.Image,
+    imageio,
+    skimage,
+    tf,
+    torch,
+    torchvision,
+]
+
 
 def library_version():
     def get_version(obj):
-        return getattr(obj, '__version__', 'None')
-    def get_name(obj):
-        return getattr(obj, '__name__', 'None')
-    print('=' * 32, 'OS info', '=' * 32)
-    print(platform.platform())
-    print('=' * 32, 'Python version', '=' * 32)
-    print(sys.version)
-    print('=' * 32, 'Libraries version', '=' * 32)
-    for _obj in [np, cv2, PIL.Image, imageio, skimage, tf, lycon, torch, torchvision, jpeg4py]:
-        print(f'{get_name(_obj)}: {get_version(_obj)}')
+        return getattr(obj, "__version__", "None")
 
+    def get_name(obj):
+        return getattr(obj, "__name__", "None")
+
+    Logger.info("=" * 32, "OS info", "=" * 32)
+    Logger.info(platform.platform())
+    Logger.info("=" * 32, "Python version", "=" * 32)
+    Logger.info(sys.version)
+    Logger.info("=" * 32, "Libraries version", "=" * 32)
+    for _obj in image_libs:
+        Logger.info(f"{get_name(_obj)}: {get_version(_obj)}")
 
 
 def _main():
@@ -44,7 +62,6 @@ def _main():
         imread_imageio,
         imread_skimage,
         imread_tf,
-        imread_lycon,
         imread_torchvision
         # imread_jpeg4py
     ]
@@ -52,17 +69,16 @@ def _main():
     # 速度計測
     loop = 300
     for i, x in enumerate(X1):
-        print('=' * 32, x.name, '=' * 32)
+        Logger.info("=" * 32, x.name, "=" * 32)
         for f in functions:
-            t = timeit.timeit('''f(x)''', number=loop, globals={'f': f, 'x': x})
-            print(
-                f'{f.__name__:15s}: {t:4.1f}[sec] (mean:{t / loop: .4f})[sec]')
+            t = timeit.timeit("""f(x)""", number=loop, globals={"f": f, "x": x})
+            Logger.info(f"{f.__name__:15s}: {t:4.1f}[sec] (mean:{t / loop: .4f})[sec]")
 
 
-def imread_opencv(path):
+def imread_opencv(path: Path) -> np.ndarray:
     arr = np.fromfile(str(path), np.uint8)
     img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
-    if img is None and path.suffix.lower() in ('.gif'):  # TODO
+    if img is None and path.suffix.lower() in (".gif"):  # TODO
         video = cv2.VideoCapture(str(path))
         _, img = video.read()
     if img is None:
@@ -70,29 +86,31 @@ def imread_opencv(path):
     return img[:, :, ::-1].astype(np.float32)
 
 
-def imread_pillow(path):
+def imread_pillow(path: Path) -> np.ndarray:
     try:
         with PIL.Image.open(path) as img:
-            if img.mode != 'RGB':
-                img = img.convert('RGB')
+            if img.mode != "RGB":
+                img = img.convert("RGB")
             return np.asarray(img, dtype=np.float32)
-    except BaseException:
+    except Exception as e:
+        Logger.error(f"failed to open {path} with error: {e}")
         return None
 
 
-def imread_imageio(path):
+def imread_imageio(path: Path) -> np.ndarray:
     try:
         img = imageio.imread(path)
         if len(img.shape) == 2:
             img = skimage.color.gray2rgb(img)
         elif img.shape[2] == 4:
             img = skimage.color.rgba2rgb(img)
-    except BaseException:
+    except BaseException as e:
+        Logger.error(f"failed to open {path} with error: {e}")
         return None
     return img.astype(np.float32)
 
 
-def imread_skimage(path):
+def imread_skimage(path: Path) -> np.ndarray:
     try:
         img = skimage.io.imread(path)
         if len(img.shape) == 2:
@@ -100,15 +118,17 @@ def imread_skimage(path):
         elif img.shape[2] == 4:
             img = skimage.color.rgba2rgb(img)
         return img.astype(np.float32)
-    except BaseException:
+    except BaseException as e:
+        Logger.error(f"failed to open {path} with error: {e}")
         return None
 
 
-def imread_tf(path):
+def imread_tf(path: Path) -> np.ndarray:
     try:
         img = tf.io.read_file(str(path))
         img = tf.image.decode_image(img, channels=3)
-    except BaseException:
+    except BaseException as e:
+        Logger.error(f"failed to open {path} with error: {e}")
         return None
     # https://www.tensorflow.org/api_docs/python/tf/io/decode_image
     # Note: decode_gif returns a 4-D array [num_frames, height, width, 3],
@@ -118,43 +138,22 @@ def imread_tf(path):
         return img.numpy().astype(np.float32)
 
 
-def imread_lycon(path):
-    try:
-        img = lycon.load(str(path))
-        # lycon can't load gif file
-        if img is None:
-            return None
-        return img.astype(np.float32)
-    except BaseException:
-        return None
-
-
-def imread_jpeg4py(path):
-    try:
-        img = jpeg4py.JPEG(str(path)).decode()
-        # lycon can't load gif file
-        if img is None:
-            return None
-        return img.astype(np.float32)
-    except BaseException:
-        return None
-
-
 def imread_torchvision(path):
     # https://pytorch.org/docs/stable/torchvision/io.html#image
     try:
         # Tensor
         tensor = torchvision.io.read_image(str(path))
         # to numpy ndarray
-        img = tensor.to('cpu').detach().numpy().copy()
+        img = tensor.to("cpu").detach().numpy().copy()
         # (3, h, w) to (w, h, 3)
         img = img.transpose(1, 2, 0)
         if img is None:
             return None
         return img.astype(np.float32)
-    except BaseException:
+    except BaseException as e:
+        Logger.error(f"failed to open {path} with error: {e}")
         return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     _main()
